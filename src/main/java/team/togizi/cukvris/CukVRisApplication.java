b6864b79e0cfd881fd128c9d5a96132402315af1
wjawksl <wjawksl@gmail.com>
2022-04-10 15:49:21 +0900

    initial commit
    2022-04-10

---
+++ src/main/java/team/togizi/cukvris/CukVRisApplication.java
@@ -0,0 +1,13 @@
+package team.togizi.cukvris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukVRisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CukVRisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/togizi/cukvris/CukVRisApplicationTests.java
@@ -0,0 +1,13 @@
+package team.togizi.cukvris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukVRisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
