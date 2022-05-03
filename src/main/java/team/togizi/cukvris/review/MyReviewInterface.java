13da9e16e06e7b05b3a6a9f8ce65e98e4abe35f5
hajin618 <1998rlagkwls@gmail.com>
2022-05-03 19:10:55 +0900

    myReview

--- src/main/java/team/togizi/cukvris/review/MyReviewInterface.java
+++ src/main/java/team/togizi/cukvris/review/MyReviewInterface.java
@@ -1,6 +1,10 @@
 package team.togizi.cukvris.review;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface MyReviewInterface {
+    @JsonProperty
     String getResName();
+    @JsonProperty
     Integer getReviewCategory();
 }
