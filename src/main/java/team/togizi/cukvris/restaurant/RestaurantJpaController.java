bf385defc92066e8247673f7c5aa444a6bc65f4f
hajin618 <1998rlagkwls@gmail.com>
2022-05-11 20:29:10 +0900

    filter

--- src/main/java/team/togizi/cukvris/restaurant/RestaurantJpaController.java
+++ src/main/java/team/togizi/cukvris/restaurant/RestaurantJpaController.java
@@ -2,10 +2,7 @@ package team.togizi.cukvris.restaurant;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,5 +34,33 @@ public class RestaurantJpaController {
         }
         return resList;
     }
+
+
+
+    // 음식점 필터로 조회
+    @GetMapping("/restaurant/filter")
+    public List<Restaurant> searchByFilter(@RequestBody List<Integer> filteredValue){
+
+        // ***findByResNameContains 부분 수정하기***
+        List<Restaurant> resFilterList=restaurantRepository.findByResNameContains("");
+
+        if(resFilterList.isEmpty()){
+            throw new ResNotFoundException(String.format("[%s] not found", filteredValue));
+        }
+
+        return resFilterList;
+    }
+
+
+    /*
+    프론트에서 데이터 받기 (JSON)
+    // 음식점 필터로 조회
+    @GetMapping("/restaurant/filter")
+    public List<Integer> searchByFilter(@RequestBody List<Integer> filteredValue){
+
+        List<Integer> resFilterList=filteredValue;
+        return resFilterList;
+    }
+    */
 
 }
--- src/main/java/team/togizi/cukvris/restaurant/RestaurantRepository.java
+++ src/main/java/team/togizi/cukvris/restaurant/RestaurantRepository.java
@@ -12,7 +12,11 @@ import java.util.Optional;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 
     //List<Restaurant> findByResNameContains(String resName);
-    
+
+    // 음식점 이름으로 검색
     @Query("SELECT r FROM restaurant r WHERE r.resName LIKE %:resName%")
     List<Restaurant> findByResNameContains(@Param("resName") String resName);
+
+    // 음식점 필터로 검색
+
 }
