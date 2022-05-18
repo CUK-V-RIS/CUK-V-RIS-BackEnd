30a029a5c91d620cd07a2a386b1a09f1ee7021c9
hajin618 <1998rlagkwls@gmail.com>
2022-05-19 02:54:46 +0900

    Retrieve all reviews

--- src/main/java/team/togizi/cukvris/review/ReviewJpaController.java
+++ src/main/java/team/togizi/cukvris/review/ReviewJpaController.java
@@ -17,9 +17,9 @@ public class ReviewJpaController {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    // 공통 (0~5)        : 재료신선, 양 많, 맛있, 가격 착, 다음에도, 가성비 좋
-    // 비건 (6~7)        : 선택지다양, 논비건과 함께
-    // 논비건 (8)        :  논비건과 비슷
+    // 공통 (1~6)        : 재료신선, 양 많, 맛있, 가격 착, 다음에도, 가성비 좋
+    // 비건 (7~8)        : 선택지다양, 논비건과 함께
+    // 논비건 (9)        :  논비건과 비슷
 
 
     // 최근 나의 리뷰 조회
@@ -43,7 +43,7 @@ public class ReviewJpaController {
     }
     */
     // @Valid 이용 유효성 검사
-    @PostMapping("/Review")
+    @PostMapping("/review")
     public ResponseEntity<Review> createReview(@Valid @RequestBody Review review){
         Review savedReview=reviewRepository.save(review);
 
@@ -52,12 +52,30 @@ public class ReviewJpaController {
                 .buildAndExpand(savedReview.getReviewIdx())
                 .toUri();
 
+        // 새로운 리뷰 생성되면 상태 코드에 200 OK 아닌 201 Created
         return ResponseEntity.created(location).build();
+
     }
 
 
     // 키워드 리뷰 조회
+    @GetMapping("/{resIdx}/review")
+    public ReviewCategory retrieveReview(@PathVariable int resIdx){
 
+        ReviewCategory reviewCategory=new ReviewCategory();
+
+        reviewCategory.setReview1_fresh(reviewRepository.retrieveAllReviews(resIdx, 1));
+        reviewCategory.setReview2_amount(reviewRepository.retrieveAllReviews(resIdx, 2));
+        reviewCategory.setReview3_taste(reviewRepository.retrieveAllReviews(resIdx, 3));
+        reviewCategory.setReview4_price(reviewRepository.retrieveAllReviews(resIdx, 4));
+        reviewCategory.setReview5_next(reviewRepository.retrieveAllReviews(resIdx, 5));
+        reviewCategory.setReview6_costPerform(reviewRepository.retrieveAllReviews(resIdx, 6));
+        reviewCategory.setReview7_option(reviewRepository.retrieveAllReviews(resIdx, 7));
+        reviewCategory.setReview8_withNone(reviewRepository.retrieveAllReviews(resIdx, 8));
+        reviewCategory.setReview9_similar(reviewRepository.retrieveAllReviews(resIdx, 9));
+
+        return reviewCategory;
+    }
 
     // 키워드 리뷰 삭제
 
--- src/main/java/team/togizi/cukvris/review/ReviewRepository.java
+++ src/main/java/team/togizi/cukvris/review/ReviewRepository.java
@@ -13,4 +13,9 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
     // 최근 나의 리뷰 조회
     @Query(value = "Select new team.togizi.cukvris.review.MyReview(res.resName, rev.reviewCategory) From restaurant res, review rev where rev.resIdx=res.resIdx and rev.userIdx= :userIdx")
     List<MyReview> findByUserIdx(@Param("userIdx") Integer userIdx);
+
+    // 음식점 전체 리뷰 조회
+    @Query(value="select count(rev) from review rev where rev.resIdx= :resIdx and rev.reviewCategory= :reviewCategory")
+    Integer retrieveAllReviews(Integer resIdx, Integer reviewCategory);
+
 }
